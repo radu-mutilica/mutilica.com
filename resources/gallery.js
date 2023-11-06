7e09398ee76edda8b10b3c248eba2dc537cde1e7
Radu Mutilica <contact@mutilica.com>
2023-11-06 19:27:09 +0200

    Fixed lazy loading issue

--- resources/gallery.js
+++ resources/gallery.js
@@ -1,80 +1,67 @@
 // Get list of images from AWS
 const photosListURL = "https://mutilica.com-photo-gallery.s3.eu-west-2.amazonaws.com/photos.json";
 
-fetch(photosListURL, {
-    mode: 'cors',
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-})
+fetch(photosListURL)
     .then(res => res.json())
-    .then((jsonContents) => {
-        console.log('Output: ', jsonContents);
-    }).catch(err => console.error(err));
+    .then((photos) => {
 
+        // Iterate over the json list of photos and generate html for 
+        // each entry, then append the newly generated html to
+        // the gallery container
+        const gallery = document.querySelector("#gallery");
 
-// Iterate over the json list of photos and generate html for 
-// each entry, then append the newly generated html to
-// the gallery container
-const gallery = document.querySelector("#gallery");
-for (var jsonPhoto of photos) {
-    console.log(jsonPhoto);
+        for (let [index, jsonPhoto] of photos.entries()) {
 
-    const newPhotoContainer = document.createElement("div");
-    newPhotoContainer.classList.add("photo-container");
+            const newLink = document.createElement("a");
+            newLink.setAttribute("href", "javascript:;");
+            newLink.classList.add("image");
+            newLink.classList.add("fit");
 
-    const newLink = document.createElement("a");
-    newLink.setAttribute("target", "_blank");
-    newLink.setAttribute("href", jsonPhoto.url);
+            const newImg = document.createElement("img");
 
-    const newImg = document.createElement("img");
+            // todo: exclude the first 20 or so images from lazy loading
+            if (index > 20) {
+                newImg.classList.add("lazy");
+                newImg.setAttribute("data-src", jsonPhoto.url);
+            } else {
+                newImg.setAttribute("src", jsonPhoto.url);
+            }
 
-    // todo: exclude the first 20 or so images from lazy loading
-    newImg.classList.add("lazy");
-    newImg.setAttribute("data-src", jsonPhoto.url);
-
-    const newDesc = document.createElement("div");
-    newDesc.classList.add("desc");
-    newDesc.innerText = jsonPhoto.desc;
-
-    newLink.appendChild(newImg);
-    newPhotoContainer.appendChild(newLink);
-    newPhotoContainer.appendChild(newDesc)
-
-    gallery.appendChild(newPhotoContainer);
-}
-
-
-
-// Set up event listeners for all of the images for lazy loading
-document.addEventListener("DOMContentLoaded", function () {
-    var lazyloadImages = document.querySelectorAll("img.lazy");
-    var lazyloadThrottleTimeout;
-
-    function lazyload() {
-        if (lazyloadThrottleTimeout) {
-            clearTimeout(lazyloadThrottleTimeout);
+            newLink.appendChild(newImg)
+            gallery.appendChild(newLink);
         }
 
-        lazyloadThrottleTimeout = setTimeout(function () {
-            var scrollTop = window.scrollY;
-            lazyloadImages.forEach(function (img) {
-                if (img.offsetTop < (window.innerHeight + scrollTop)) {
-                    img.src = img.dataset.src;
-                    img.classList.remove('lazy');
+        // Set up event listeners for all of the images for lazy loading
+        document.addEventListener("DOMContentLoaded", function () {
+            var lazyloadImages = document.querySelectorAll("img.lazy");
+            var lazyloadThrottleTimeout;
+
+            function lazyload() {
+                if (lazyloadThrottleTimeout) {
+                    clearTimeout(lazyloadThrottleTimeout);
                 }
-            });
-            if (lazyloadImages.length == 0) {
-                document.removeEventListener("scroll", lazyload);
-                window.removeEventListener("resize", lazyload);
-                window.removeEventListener("orientationChange", lazyload);
+
+                lazyloadThrottleTimeout = setTimeout(function () {
+                    var scrollTop = window.scrollY;
+                    lazyloadImages.forEach(function (img) {
+                        if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                            img.src = img.dataset.src;
+                            img.classList.remove('lazy');
+                        }
+                    });
+                    if (lazyloadImages.length == 0) {
+                        document.removeEventListener("scroll", lazyload);
+                        window.removeEventListener("resize", lazyload);
+                        window.removeEventListener("orientationChange", lazyload);
+                    }
+                }, 20);
             }
-        }, 20);
-    }
 
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-});
+            document.addEventListener("scroll", lazyload);
+            window.addEventListener("resize", lazyload);
+            window.addEventListener("orientationChange", lazyload);
+        });
 
+
+    }).catch(err => console.error(err));
 
