3823330231fa8d43d3b63d34da58cbd3f6bc4e93
Radu Mutilica <contact@mutilica.com>
2023-11-08 16:41:20 +0200

    Added lightbox to gallery

--- resources/gallery.js
+++ resources/gallery.js
@@ -1,67 +1,39 @@
 // Get list of images from AWS
 const photosListURL = "https://mutilica.com-photo-gallery.s3.eu-west-2.amazonaws.com/photos.json";
 
-fetch(photosListURL)
-    .then(res => res.json())
-    .then((photos) => {
+const buildGallery = async function () {
+    fetch(photosListURL)
+        .then(res => res.json())
+        .then((photos) => {
 
-        // Iterate over the json list of photos and generate html for 
-        // each entry, then append the newly generated html to
-        // the gallery container
-        const gallery = document.querySelector("#gallery");
+            // Iterate over the json list of photos and generate html for 
+            // each entry, then append the newly generated html to
+            // the gallery container
+            const gallery = document.querySelector("#main-gallery");
 
-        for (let [index, jsonPhoto] of photos.entries()) {
+            for (let [index, jsonPhoto] of photos.entries()) {
 
-            const newLink = document.createElement("a");
-            newLink.setAttribute("href", "javascript:;");
-            newLink.classList.add("image");
-            newLink.classList.add("fit");
+                const newLink = document.createElement("a");
+                newLink.setAttribute("href", jsonPhoto.url);
+                newLink.classList.add("image");
+                newLink.classList.add("fit");
 
-            const newImg = document.createElement("img");
+                const newImg = document.createElement("img");
+                newImg.setAttribute("src", jsonPhoto.thumb);
 
-            // todo: exclude the first 20 or so images from lazy loading
-            if (index > 20) {
-                newImg.classList.add("lazy");
-                newImg.setAttribute("data-src", jsonPhoto.url);
-            } else {
-                newImg.setAttribute("src", jsonPhoto.url);
+                newLink.appendChild(newImg)
+                gallery.appendChild(newLink);
             }
+            console.log("Finished building gallery...")
+            var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+            return photos;
+        }).catch(err => console.error(err));
+}
 
-            newLink.appendChild(newImg)
-            gallery.appendChild(newLink);
-        }
+const start = async function () {
+    const result = await buildGallery();
 
-        // Set up event listeners for all of the images for lazy loading
-        document.addEventListener("DOMContentLoaded", function () {
-            var lazyloadImages = document.querySelectorAll("img.lazy");
-            var lazyloadThrottleTimeout;
+}
 
-            function lazyload() {
-                if (lazyloadThrottleTimeout) {
-                    clearTimeout(lazyloadThrottleTimeout);
-                }
-
-                lazyloadThrottleTimeout = setTimeout(function () {
-                    var scrollTop = window.scrollY;
-                    lazyloadImages.forEach(function (img) {
-                        if (img.offsetTop < (window.innerHeight + scrollTop)) {
-                            img.src = img.dataset.src;
-                            img.classList.remove('lazy');
-                        }
-                    });
-                    if (lazyloadImages.length == 0) {
-                        document.removeEventListener("scroll", lazyload);
-                        window.removeEventListener("resize", lazyload);
-                        window.removeEventListener("orientationChange", lazyload);
-                    }
-                }, 20);
-            }
-
-            document.addEventListener("scroll", lazyload);
-            window.addEventListener("resize", lazyload);
-            window.addEventListener("orientationChange", lazyload);
-        });
-
-
-    }).catch(err => console.error(err));
+start();
 
